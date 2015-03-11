e8a556642511cb734f79a063c817b911fe49ec5b
Jim Kalafut <jim@kalafut.net>
2015-03-10 21:46:36 -0700

    Increase pointer use to simplfy interface

--- todotxt.go
+++ todotxt.go
@@ -19,7 +19,7 @@ import (
 
 // TaskList represents a list of todo.txt task entries.
 // It is usually loaded from a whole todo.txt file.
-type TaskList []Task
+type TaskList []*Task
 
 // IgnoreComments can be set to 'false', in order to revert to a more standard todo.txt behaviour.
 // The todo.txt format does not define comments.
@@ -53,15 +53,15 @@ func (tasklist *TaskList) AddTask(task *Task) {
 	}
 	task.Id += 1
 
-	*tasklist = append(*tasklist, *task)
+	*tasklist = append(*tasklist, task)
 }
 
 // GetTask returns a Task by given task 'id' from the TaskList. The returned Task pointer can be used to update the Task inside the TaskList.
 // Returns an error if Task could not be found.
-func (tasklist *TaskList) GetTask(id int) (*Task, error) {
-	for i := range *tasklist {
-		if ([]Task(*tasklist))[i].Id == id {
-			return &([]Task(*tasklist))[i], nil
+func (tasklist TaskList) GetTask(id int) (*Task, error) {
+	for i := range tasklist {
+		if tasklist[i].Id == id {
+			return tasklist[i], nil
 		}
 	}
 	return nil, errors.New("task not found")
@@ -111,7 +111,7 @@ func (tasklist *TaskList) RemoveTask(task Task) error {
 
 // Filter filters the current TaskList for the given predicate (a function that takes a task as input and returns a bool),
 // and returns a new TaskList. The original TaskList is not modified.
-func (tasklist *TaskList) Filter(predicate func(Task) bool) *TaskList {
+func (tasklist *TaskList) Filter(predicate func(*Task) bool) *TaskList {
 	var newList TaskList
 	for _, t := range *tasklist {
 		if predicate(t) {
@@ -127,7 +127,7 @@ func (tasklist *TaskList) Filter(predicate func(Task) bool) *TaskList {
 //
 // Note: This will clear the current TaskList and overwrite it's contents with whatever is in *os.File.
 func (tasklist *TaskList) LoadFromFile(file *os.File) error {
-	*tasklist = []Task{} // Empty tasklist
+	*tasklist = []*Task{} // Empty tasklist
 
 	taskId := 1
 	scanner := bufio.NewScanner(file)
@@ -145,7 +145,7 @@ func (tasklist *TaskList) LoadFromFile(file *os.File) error {
 		}
 		task.Id = taskId
 
-		*tasklist = append(*tasklist, *task)
+		*tasklist = append(*tasklist, task)
 		taskId++
 	}
 	if err := scanner.Err(); err != nil {
